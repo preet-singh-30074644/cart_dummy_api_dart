8d57e32f68ce388191ce5fe038cf78924478aac4
preet-singh-30074644 <preet.singh@adani.com>
2023-06-06 16:14:26 +0530

    done: all apis

--- bin/server.dart
+++ bin/server.dart
@@ -5,11 +5,19 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import 'data/data.dart';
+import 'model/product_model/product_model.dart';
+import 'package:collection/collection.dart';
+
 // Configure routes.
 final _router = Router()
   ..get('/', _rootHandler)
   ..get('/echo/<message>', _echoHandler)
-  ..post('/login', login);
+  ..post('/login', login)
+  ..get('/products', getProducts)
+  ..post('/products', addProducts)
+  ..post('/cart', addToCart)
+  ..get('/cart', getCart);
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!\n');
@@ -36,9 +44,51 @@ void main(List<String> args) async {
 Future<Response> login(Request req) async {
   final data = jsonDecode(await req.readAsString());
   if (data['password'] != null) {
-    return Response.ok(jsonEncode({
-      "success": true,
-    }));
+    return Response.ok(
+        jsonEncode({
+          "success": true,
+        }),
+        headers: {'Content-type': 'application/json'});
   }
-  return Response.badRequest(body: jsonEncode({"error": "bad request"}));
+  return Response.badRequest(
+      body: jsonEncode({"error": "bad request"}),
+      headers: {'Content-type': 'application/json'});
+}
+
+Future<Response> addProducts(Request req) async {
+  final data = jsonDecode(await req.readAsString());
+  final product = ProductModel.fromJson(data);
+  final doesContain = products.firstWhereOrNull(
+    (element) => element.productId == product.productId,
+  );
+  if (doesContain == null) {
+    products.add(product);
+    return Response.ok(jsonEncode({"success": true}),
+        headers: {'Content-type': 'application/json'});
+  }
+  return Response.badRequest();
+}
+
+Future<Response> addToCart(Request req) async {
+  final data = jsonDecode(await req.readAsString());
+  final productId = data['productId'];
+  final product = products.firstWhereOrNull(
+    (element) => element.productId == productId,
+  );
+  if (product != null) {
+    globalCart.add(product);
+    return Response.ok(jsonEncode({"success": true}),
+        headers: {'Content-type': 'application/json'});
+  }
+  return Response.badRequest();
+}
+
+Response getProducts(Request req) {
+  return Response.ok(jsonEncode({"products": products}),
+      headers: {'Content-type': 'application/json'});
+}
+
+Response getCart(Request req) {
+  return Response.ok(jsonEncode({"cartItems": globalCart}),
+      headers: {'Content-type': 'application/json'});
 }
